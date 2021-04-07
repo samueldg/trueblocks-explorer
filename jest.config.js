493b52e7e343f02dea198bd70fe2a173ed7ccfbc
Dawid Szlachta <dawids@fr24.com>
2021-04-07 23:16:33 +0200

    Fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   // testMatch: ['src/*.test.ts', 'src/**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/cypress/'],
+  testPathIgnorePatterns: ['/node_modules/', '/cypress/', '/dist/'],
   moduleNameMapper: {
     '@common/(.*)': ['<rootDir>/src/common/$1'],
   },
