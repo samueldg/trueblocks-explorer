0e27dd1df65734d32d2a901555901edad6f7f031
tjayrush <jrush@quickblocks.io>
2021-05-22 17:05:54 -0400

    Adding loading to BlocksView (probably wrongly)

--- src/ui/views/Explorer/Blocks.tsx
+++ src/ui/views/Explorer/Blocks.tsx
@@ -1,20 +1,26 @@
 import { PageHeader, Tabs } from 'antd';
 import React, { useCallback } from 'react';
+import { createErrorNotification } from '@modules/error_notification';
+import { Loading } from '@components/Loading';
 import { useCommand } from '../../hooks/useCommand';
 
 const { TabPane } = Tabs;
 
 export const BlocksView = () => {
-  const [data] = useCommand('blocks', { blocks: 'latest', hashes: true, cache: true });
-  const [detail] = useCommand('blocks', { blocks: 'latest', cache: true });
+  const [data, loading1] = useCommand('blocks', { blocks: 'latest', hashes: true, cache: true });
+  const [detail, loading2] = useCommand('blocks', { blocks: 'latest', cache: true });
   const getData = useCallback((response) => (response.status === 'fail' ? [] : response.content), []);
-
+  if (data.status === 'fail') {
+    createErrorNotification({
+      description: 'Could not fetch blocks',
+    });
+  }
   const theItem = getData(data);
   const theDetail = getData(detail);
   const title = `Exploring transaction [${theItem}]`;
 
   return (
-    <>
+    <Loading loading={loading1 && loading2}>
       <PageHeader title={title} />
       <Tabs>
         <TabPane tab="Summary" key="summary">
@@ -24,6 +30,6 @@ export const BlocksView = () => {
           <pre>{JSON.stringify(theDetail, null, 2)}</pre>
         </TabPane>
       </Tabs>
-    </>
+    </Loading>
   );
 };
