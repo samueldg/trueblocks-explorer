b50134ff8b46ad05211c3c04c62bfabf79ed921c
tjayrush <jrush@quickblocks.io>
2021-05-23 06:01:04 -0400

    Moved System to Settings

--- src/ui/views/Settings/Settings.tsx
+++ src/ui/views/Settings/Settings.tsx
@@ -5,19 +5,18 @@ import { Status } from './Status';
 import { Tags } from './Tags';
 import { Signatures } from './Signatures';
 import { Caches } from './Caches';
-import { Other } from './Other';
 import { Skins } from './Skins';
 import { Schemas } from './Schemas';
 
 const { TabPane } = Tabs;
 
 export const SystemView = () => {
-  const title = 'System';
+  const title = 'Settings';
   return (
     <>
       <PageHeader title={title} />
       <Tabs>
-        <TabPane tab="System Status" key="status">
+        <TabPane tab="Status" key="status">
           <Status />
         </TabPane>
         <TabPane tab="Tags" key="tags">
@@ -28,9 +27,6 @@ export const SystemView = () => {
         </TabPane>
         <TabPane tab="Caches" key="caches">
           <Caches />
-        </TabPane>
-        <TabPane tab="Other" key="other">
-          <Other />
         </TabPane>
         <TabPane tab="Skins" key="skins">
           <Skins />
--- src/ui/views/System/Other.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Other = () => (
-  <h4>Other</h4>
-);
