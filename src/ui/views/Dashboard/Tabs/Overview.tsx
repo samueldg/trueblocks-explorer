5f30e7cd90bd5aa42d157a5d1a50eae8bbfac0fa
tjayrush <jrush@quickblocks.io>
2021-06-07 23:38:10 -0400

    Use cards in the dashboard overview

--- src/ui/views/Dashboard/Tabs/Overview.tsx
+++ src/ui/views/Dashboard/Tabs/Overview.tsx
@@ -1,14 +1,40 @@
 import { createErrorNotification } from '@modules/error_notification';
-import { Button, notification } from 'antd';
+import { Button, Card, Col, notification, Row } from 'antd';
 import React from 'react';
+import { createUseStyles } from 'react-jss';
 
 const showError = () => notification.error(createErrorNotification({ description: 'Just testing' }));
 
-export const Overview = () => (
+const useStyles = createUseStyles({
+  card: {
+    margin: '2px',
+    border: '1px brown dashed',
+    width: 300,
+    height: 400
+  },
+});
+
+export const Overview = () => {
+  const styles = useStyles();
+  return (
   <>
-    Welcome to TrueBlocks Explorer
+  <Row>
+    <Col>
+    <Card hoverable title="Balance Histories" extra={<a href="#">More...</a>} className={styles.card}>A chart of the history of balances in eth, tokens, dollars</Card>
+    </Col>
+    <Col>
+    <Card hoverable title="Monitors" extra={<a href="#">More...</a>} className={styles.card}>A list of all monitored addresses</Card>
+    </Col>
+    <Col>
+    <Card hoverable title="Alerts" extra={<a href="#">More...</a>} className={styles.card}>A list of active alerts</Card>
+    </Col>
+    <Col>
+    <Card hoverable title="Neighbors" extra={<a href="#">More...</a>} className={styles.card}>A graph of neighboring addresses</Card>
+    </Col>
+    </Row>
+    <br />
     <Button type='primary' onClick={showError}>
       Error
     </Button>
   </>
-);
+)};
