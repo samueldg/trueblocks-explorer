f5309066be79184452fa9576ed99dbc50ebd644c
tjayrush <jrush@quickblocks.io>
2021-06-16 20:10:52 -0400

    Cleanup

--- src/ui/views/Dashboard/Tabs/Accounts/Tabs/Functions.tsx
+++ src/ui/views/Dashboard/Tabs/Accounts/Tabs/Functions.tsx
@@ -4,16 +4,15 @@ import { createErrorNotification } from '@modules/error_notification';
 import React, { useCallback } from 'react';
 
 export const AccountFunctions = () => {
-  const [data, loading] = useCommand('export', { addrs: '0xf503017d7baf7fbc0fff7492b751025c6a78179b', fmt: 'json' });
-  const getData = useCallback((response) => (response.status === 'fail' ? [] : response.data), []);
-
-  const theItem = getData(data);
-  if (data.status === 'fail') {
+  const [functions, loading] = useCommand('export', { addrs: '0xf503017d7baf7fbc0fff7492b751025c6a78179b', fmt: 'json' });
+  if (functions.status === 'fail') {
     createErrorNotification({
       description: 'Could not fetch indexes',
     });
   }
 
+  const getData = useCallback((response) => (response.status === 'fail' ? [] : response.data), []);
+  const theItem = getData(functions);
   return (
     <Loading loading={loading}>
       <pre>{JSON.stringify(theItem, null, 2)}</pre>
