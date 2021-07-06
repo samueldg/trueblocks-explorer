fc290ecea7a3b419424f8e49a8b853bc0311e87e
tjayrush <jrush@quickblocks.io>
2021-07-05 21:27:56 -0400

    Cleanup

--- src/ui/views/Settings/Tabs/Skins.tsx
+++ src/ui/views/Settings/Tabs/Skins.tsx
@@ -3,19 +3,18 @@ import { createUseStyles } from 'react-jss';
 import useGlobalState from '../../../state';
 
 const themeList = [
-  { theme: 'Blue on Black', primaryColor: 'lightblue', secondaryColor: 'black', outerBorder: '1px solid pink' },
-  { theme: 'Yellow on Green', primaryColor: 'yellow', secondaryColor: 'forestgreen', outerBorder: '1px dashed red' },
-  { theme: 'Orange on Black', primaryColor: 'orange', secondaryColor: 'black', outerBorder: '1px solid lightgrey' },
+  { theme: 'Blue on Black', primaryColor: 'lightblue', secondaryColor: 'black' },
+  { theme: 'Yellow on Green', primaryColor: 'yellow', secondaryColor: 'forestgreen' },
+  { theme: 'Orange on Black', primaryColor: 'orange', secondaryColor: 'black' },
 ];
 
 const useStyles = createUseStyles({
-  skinContainer: (theme: any) => ({
+  skinContainer: {
     display: 'flex',
     alignItems: 'center',
     marginTop: '16px',
     cursor: 'pointer',
-    border: `${theme.outerBorder}`,
-  }),
+  },
   skinItem: {
     width: '400px',
     padding: '8px',
@@ -33,19 +32,9 @@ export const Skins = () => {
     <div>
       <h2 className={styles.skinTitle}>Skins</h2>
       {themeList.map((theme) => (
-        <div
-          style={{ border: theme.outerBorder, width: '400px', verticalAlign: 'top' }}
-          key={theme.theme}
-          className={styles.skinContainer}
-          onClick={() => setTheme(theme)}>
+        <div key={theme.theme} className={styles.skinContainer} onClick={() => setTheme(theme)}>
           <div style={{ backgroundColor: theme.secondaryColor }} className={styles.skinItem}>
             <div style={{ color: theme.primaryColor }}>{theme.theme}</div>
-          </div>
-          <div>
-            <br />
-            <br />
-            <br />
-            <br />
           </div>
         </div>
       ))}
