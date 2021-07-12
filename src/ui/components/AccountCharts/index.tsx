2d86e1880264dd57ef8189afb9fc835aef17cc16
Dan <dee.aye.en@gmail.com>
2021-07-12 13:42:58 +0300

    all unique assets are now mapped

--- src/ui/components/AccountCharts/index.tsx
+++ src/ui/components/AccountCharts/index.tsx
@@ -1,67 +1,47 @@
 import React from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import moment from 'moment';
-
-const testData = [
-  {
-    name: 'Page A',
-    uv: 100,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 100,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { chartColors } from './colors';
 
 export default function AccountCharts({ data }: { data: any }) {
+  const uniqueAssets: any = [];
+
   const historyETH: any = [];
   const historyDAI: any = [];
 
   data &&
-    data?.data.map((item: any) => {
-      const statementETH = item.statements?.find((s: any) => s.assetSymbol === 'ETH');
+    data.data.map((item: any) => {
+      item.statements?.map((asset: any) => {
+        if (uniqueAssets.find((a: any) => a.asset === asset.assetSymbol) === undefined) {
+          uniqueAssets.push({
+            asset: asset.assetSymbol,
+            data: [],
+            color: chartColors[Math.floor(Math.random() * (9 - 0 + 1) + 0)],
+          });
+        }
+      });
+      uniqueAssets.map((record: any, i: number) => {
+        const foundStatement = item.statements?.find((s: any) => s.assetSymbol === record.asset);
+        if (foundStatement) {
+          uniqueAssets[i].data = [
+            ...uniqueAssets[i].data,
+            { endBal: foundStatement.endBal, date: new Date(foundStatement.timestamp * 1000) },
+          ];
+        }
+      });
+      /*const statementETH = item.statements?.find((s: any) => s.assetSymbol === 'ETH');
       const statementDAI = item.statements?.find((s: any) => s.assetSymbol === 'DAI');
       if (statementETH) {
         historyETH.push({ endBal: statementETH.endBal, date: new Date(statementETH.timestamp * 1000) });
       }
       if (statementDAI) {
         historyDAI.push({ endBal: statementDAI.endBal, date: new Date(statementETH.timestamp * 1000) });
-      }
+      }*/
     });
+
+  /*data && uniqueAssets.length > 0 && data.data.map((item: any) => {
+      item.statements?.map()
+    })*/
 
   const testDataETH = historyETH.map((item: any) => {
     return {
@@ -79,48 +59,42 @@ export default function AccountCharts({ data }: { data: any }) {
 
   return (
     <div>
-      <div style={{ marginBottom: '24px', fontSize: '28px', fontWeight: 'bold' }}>ETH</div>
-      <div style={{ width: '100%', height: '300px' }}>
-        <ResponsiveContainer width='100%' height='100%'>
-          <AreaChart
-            width={500}
-            height={400}
-            data={testDataETH}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}>
-            <CartesianGrid strokeDasharray='3 3' />
-            <XAxis dataKey='name' />
-            <YAxis />
-            <Tooltip />
-            <Area type='monotone' dataKey='ETH' stackId='1' stroke='#8884d8' fill='#8884d8' />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-      <div style={{ marginBottom: '24px', fontSize: '28px', fontWeight: 'bold' }}>DAI</div>
-      <div style={{ width: '100%', height: '300px' }}>
-        <ResponsiveContainer width='100%' height='100%'>
-          <AreaChart
-            width={500}
-            height={400}
-            data={testDataDAI}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}>
-            <CartesianGrid strokeDasharray='3 3' />
-            <XAxis dataKey='name' />
-            <YAxis />
-            <Tooltip />
-            <Area type='monotone' dataKey='DAI' stackId='1' stroke='#82ca9d' fill='#82ca9d' />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
+      {uniqueAssets.map((asset: any, i: number) => (
+        <>
+          <div style={{ marginBottom: '24px', fontSize: '28px', fontWeight: 'bold' }}>{asset.asset}</div>
+          <div style={{ width: '100%', height: '300px' }}>
+            <ResponsiveContainer width='100%' height='100%'>
+              <AreaChart
+                width={500}
+                height={400}
+                data={asset.data.map((item: any) => {
+                  return {
+                    name: moment(item.date).format('MM/DD/YYYY'),
+                    [asset.asset]: parseFloat(item.endBal || 0),
+                  };
+                })}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}>
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis dataKey='name' />
+                <YAxis />
+                <Tooltip />
+                <Area
+                  type='monotone'
+                  dataKey={asset.asset}
+                  stackId='1'
+                  stroke={chartColors[i] || '#63b598'}
+                  fill={chartColors[i] || '#63b598'}
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
+        </>
+      ))}
     </div>
   );
 }
