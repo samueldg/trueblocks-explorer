96e63c6200a364210d5be0aba5421b25ea396e3c
tjayrush <jrush@quickblocks.io>
2021-07-15 06:19:44 -0400

    Starting function view

--- src/ui/views/Dashboard/Tabs/Accounts/LeftTabs/Functions.tsx
+++ src/ui/views/Dashboard/Tabs/Accounts/LeftTabs/Functions.tsx
@@ -1,4 +1,5 @@
 import { ReconciliationArray, TransactionArray } from '@modules/types';
+
 import React from 'react';
 
 export const Functions = ({
@@ -10,5 +11,20 @@ export const Functions = ({
   loading: boolean;
   accountAddress: string;
 }) => {
-  return <div>Functions</div>;
+  if (!theData) return <></>;
+
+  const counts = Object.create(null);
+  theData.forEach((item: any, i: number) => {
+    if (!counts[item.articulatedTx?.name]) counts[item.articulatedTx?.name] = 1;
+    else counts[item.articulatedTx?.name] = Number(counts[item.articulatedTx?.name]) + 1;
+  });
+  Object.keys(counts).map((item: any) => {
+    console.log(item);
+  });
+
+  // uniqueFuncs.keys.sort(function (a: any, b: any) {
+  //   return b - a;
+  // });
+
+  return <pre>{JSON.stringify(counts, null, 2)}</pre>;
 };
